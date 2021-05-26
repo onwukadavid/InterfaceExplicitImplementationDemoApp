c064c7249103a9f56670ca9db60276212f9146a4
onwukadavid <onwukadavid21@gmail.com>
2021-05-26 16:19:15 +0100

    Focused on Casting

--- InterfaceExplicitImplementationDemoApp/Program.cs
+++ InterfaceExplicitImplementationDemoApp/Program.cs
@@ -34,11 +34,16 @@ class program
     static void Main(string[] args)
     {
         IA1 a1; IA2 a2; CA a;
+        a = new CA();
+
+        a1 = a;     //Implicit
+
+        //  a = a1;     //IInalid
+        a = (CA)a1;  //Explicit
+
         a1 = new CA();
         a2 = new CA();
-        a = new CA();
-        a1.Foo2();
-        a2.Foo2();
-       a.Foo2();
+        a2 = (IA2)a1;   //a1 must refer to object of class which has also implemented IA2 - else InvalidCastException will be thrown
+        a1 = (IA1)a2;
     }
 }
